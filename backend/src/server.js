d036dd8c42612531b9e89cb16e28d967143be373
Juwayyed <Juwayyed@gmail.com>
2026-06-02 22:12:38 +0200

    Middleware and Ratelimiter

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,19 @@ import express from "express";
 import memoRoutes from "./routes/memoRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4001;
 
-connectDB();
 app.use(express.json());
+app.use(rateLimiter);
 app.use("/api/memos", memoRoutes);
 
-app.listen(PORT, () => {
-  console.log("Server started on port:", PORT);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on port:", PORT);
+  });
 });
